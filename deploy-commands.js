4e053bd38af3e1aa921b687e3a48cb51aad7ac44
fiddy-fiddy <50fifty50split@gmail.com>
2026-01-19 20:23:18 -0500

    deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,27 @@
+const { REST, Routes } = require('discord.js');
+require('dotenv').config();
+
+const commands = [
+{
+name: 'ping',
+description: 'Replies with Pong!',
+},
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+try {
+console.log('Registering slash commands...');
+await rest.put(
+Routes.applicationGuildCommands(
+process.env.CLIENT_ID,
+process.env.GUILD_ID
+),
+{ body: commands }
+);
+console.log('Slash commands registered.');
+} catch (error) {
+console.error(error);
+}
+})();
